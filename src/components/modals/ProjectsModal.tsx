f6f1fde0663c07917aeb0c66e1a7031ea2ef0527
yeshu10 <yeshvigupta10@gmail.com>
2025-06-04 02:50:06 +0530

    project modal updated

--- src/components/modals/ProjectsModal.tsx
+++ src/components/modals/ProjectsModal.tsx
@@ -83,7 +83,8 @@ export default function ProjectsModal({ projects, onClose }: ProjectsModalProps)
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-25" />
+          <div className="fixed inset-0  bg-opacity-10 transition-opacity" />
+          
         </Transition.Child>
 
         <div className="fixed inset-0 overflow-y-auto">
